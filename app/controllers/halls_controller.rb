d126ec2bbbff2a06e6f0effc2ce14e4b3f278d11
martynaj2 <martyna.jasiak@gmail.com>
2018-07-16 22:54:43 +0200

    [add][update] add halls show action, update halls controller

--- app/controllers/halls_controller.rb
+++ app/controllers/halls_controller.rb
@@ -4,4 +4,14 @@ class HallsController < ApplicationController
     @halls = Hall.all
   end
 
+  def show
+    @hall = Hall.find(params[:id])
+  end
+
+  private
+
+  def hall_params
+      params.require(:hall).permit(:title, :capacity)
+  end
+
 end
