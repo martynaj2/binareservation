8c8b114d47dab293fdb995db8b46eda305d912e5
ptoiraleus <ptoiraleus@gmail.com>
2018-07-17 11:56:04 +0200

    [add] Edit button for admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,26 @@ class UsersController < ApplicationController
 	def destroy
 	    @user = User.find(params[:id])
 	    @user.destroy
-	  end
+	end
+
+	def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to '/users#index', notice: 'User Updated'
+    else
+      render :edit
+    end
+  end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :surname, :email)
+		#params.require(:user).permit(:name, :surname, :email, :password, :password_confirmation)
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
 
 	resources :halls
 	devise_for :users, :path_prefix => 'my'
-	match '/users',   to: 'users#index',   via: 'get'
-	match '/users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
-	# resources :users
+
+	resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
