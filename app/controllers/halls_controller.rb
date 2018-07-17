e3bdcbce65eb4fcaba02be100b8f3c127ba20254
martynaj2 <martyna.jasiak@gmail.com>
2018-07-17 11:01:03 +0200

    [add]add new hall action, alerts

--- app/controllers/halls_controller.rb
+++ app/controllers/halls_controller.rb
@@ -1,7 +1,20 @@
 class HallsController < ApplicationController
 
+  def new
+    @hall = Hall.new
+  end
+
   def index
     @halls = Hall.all
+  end
+
+  def create
+    @hall = Hall.new(hall_params)
+      if @hall.save
+        redirect_to halls_path, notice: 'Room created'
+      else
+        redirect_to halls_path, alert: 'Something went wrong'
+      end
   end
 
   def show
@@ -13,5 +26,7 @@ class HallsController < ApplicationController
   def hall_params
       params.require(:hall).permit(:title, :capacity)
   end
+
+
 
 end
