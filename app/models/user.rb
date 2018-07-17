da67670e0414c5bfafb8d0550fa8c7ae4b3072f7
ptoiraleus <ptoiraleus@gmail.com>
2018-07-17 11:08:34 +0200

    [add] user list for admin, and chenged navbar, so it wouldn't show sign in and register for logged user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+ def fullname
+   "#{name} #{surname}"
+ end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 	root 'home#welcome'
 
 	resources :halls
+	match '/users',   to: 'users#index',   via: 'get'
   devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
