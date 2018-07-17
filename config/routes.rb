1cb31b4ff6fa8ffff52838fbf529e2013e237359
ptoiraleus <ptoiraleus@gmail.com>
2018-07-17 14:12:59 +0200

    minor refactor

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
 
 	resources :users
 	resources :verify
-
-	#get 'verify', to: 'verify#index'
+	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
