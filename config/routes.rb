8d13bb25cf950872714dd4548abf349cc288ac51
martynaj2 <martyna.jasiak@gmail.com>
2018-07-17 14:25:42 +0200

    [fix] fix issue with saving new room

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
 	root 'home#welcome'
+
+	devise_for :users, :path_prefix => 'my'
 	resources :reservations
 	resources :halls
-	devise_for :users, :path_prefix => 'my'
-
 	resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
