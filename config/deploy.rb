02d916049903816ebb9968fbaf9f7c88a032079f
martynaj2 <martyna.jasiak@gmail.com>
2018-07-18 15:12:59 +0200

    [fix] git url

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "binarmeets"
-set :repo_url, "git@github.com/martynaj2/binareservation"
+set :repo_url, "git@github.com/martynaj2/binareservation.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
