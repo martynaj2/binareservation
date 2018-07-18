aaf43fb768fef18fcc5298e9c9391b4c3247ce76
ptoiraleus <ptoiraleus@gmail.com>
2018-07-18 16:55:54 +0200

    [fix] Fixed mailer

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
 
 	before_action :configure_permitted_parameters, :only => [:create]
 
-	after_action :send_admin_mail, :only => [:sign_up]
+	after_action :send_admin_mail, :only => [:create]
 
   def send_admin_mail
     UserMailer.hello_mail(current_user).deliver_later
