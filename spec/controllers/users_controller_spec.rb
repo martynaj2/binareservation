1c78fe7a9bfa79d4365e41e71478fee38b529da9
martynaj2 <martyna.jasiak@gmail.com>
2018-07-20 12:02:10 +0200

    [add] controller test for action index

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+  let(:user) { create(:user) }
+  before { sign_in(user) }
+
+  describe '#index' do
+    it 'should return successful response' do
+        get :index
+        expect(response).to be_successful
+    end
+  end
+
+  it 'should render index template' do
+    get :index
+    expect(response).to render_template('index')
+  end
+
 end
