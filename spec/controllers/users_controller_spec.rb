ba105da021009cef5fd727aa9ee487655066648d
martynaj2 <martyna.jasiak@gmail.com>
2018-07-20 12:24:58 +0200

    [update] finish test for index

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-  let(:user) { create(:user) }
-  before { sign_in(user) }
+   let(:user1) { create(:user, email: 'b@wp.pl') }
+   before { sign_in(user1) }
+
+   let(:user2) { create(:user, email: 'c@wp.pl') }
+   before { sign_in(user2) }
 
   describe '#index' do
     subject { get :index }
@@ -12,6 +15,14 @@ RSpec.describe UsersController, type: :controller do
       before { subject }
       it { expect(response).to be_successful }
       it { expect(response).to render_template('index') }
+    end
+
+    context 'users' do
+      
+      it 'should return all users' do
+        subject
+        expect(assigns(:users)).to match_array([user1, user2])
+      end
     end
 
   end
