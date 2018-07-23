cd2658704d83f4e19f6951009f2a900ec58b78de
MrArthurr <chrzaszczewski.artur@gmail.com>
2018-07-23 10:23:54 +0200

    [remove] deleted everything connected with description

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -13,7 +13,6 @@ end
 class Reservation < ActiveRecord::Base
 
   validates :title, presence: true, length: { minimum: 5}
-  validates :description, presence: true, length: { minimum: 10 }
   validates :number_of_people, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :reservation do
     title             Faker::FamilyGuy.character
-    description       Faker::FamilyGuy.quote
     number_of_people  Faker::Number.between(5, 20)
     start_date        Time.now
     end_date          Time.now + 1.hour
