88673e8e7921df905b1b44f48d83525ebb20db49
MrArthurr <chrzaszczewski.artur@gmail.com>
2018-07-23 12:00:01 +0200

    rename invited to invited_ids and inviter to inviter_ids in group model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_23_085441) do
+ActiveRecord::Schema.define(version: 2018_07_23_093819) do
 
   create_table "group_users", force: :cascade do |t|
     t.integer "user_id"
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(version: 2018_07_23_085441) do
 
   create_table "groups", force: :cascade do |t|
     t.string "title"
-    t.integer "inviter"
-    t.integer "invited"
+    t.integer "inviter_ids"
+    t.integer "invited_ids"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2018_07_23_085441) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "hall_id"
-    t.integer "invited"
+    t.integer "invited_ids"
   end
 
   create_table "users", force: :cascade do |t|
@@ -58,8 +58,8 @@ ActiveRecord::Schema.define(version: 2018_07_23_085441) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "surname"
-    t.boolean "is_admin"
-    t.boolean "is_premium"
+    t.boolean "admin"
+    t.boolean "premium"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(version: 2018_07_23_085441) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.boolean "is_verified"
+    t.boolean "verified"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
