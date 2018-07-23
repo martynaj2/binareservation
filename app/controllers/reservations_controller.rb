092cbcec03ce37f97ffa66b3af9fe7096f3fd564
ptoiraleus <ptoiraleus@gmail.com>
2018-07-23 14:04:57 +0200

    [merge]

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -21,8 +21,8 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.find(params[:id])
     current_reservation = current_user.reservations.build(reservation_params)
     reservations = Reservation.where(hall_id: @reservation.hall_id).where.not(id: params[:id])
-    conflicting_reservations = Reservation.conflict_validation(reservations, current_reservation)
-    if conflicting_reservations.empty?
+    @conflicting_reservations = Reservation.conflict_validation(reservations, current_reservation)
+    if @conflicting_reservations.empty?
        if @reservation.update(reservation_params)
          redirect_to reservations_path, notice: 'Reservation Updated'
        else
@@ -36,15 +36,15 @@ class ReservationsController < ApplicationController
   def create
     @reservation = current_user.reservations.build(reservation_params)
     reservations = Reservation.where(hall_id: @reservation.hall_id)
-    conflicting_reservations = Reservation.conflict_validation(reservations, @reservation)
-    if conflicting_reservations.empty?
+    @conflicting_reservations = Reservation.conflict_validation(reservations, @reservation)
+    if @conflicting_reservations.empty?
        if @reservation.save
          redirect_to reservations_path, notice: 'Reservation was created.'
        else
          redirect_to reservations_path, alert: "Something went wrong #{@reservation.errors.full_messages}"
        end
     else
-      premium_override(conflicting_reservations)
+      premium_override
     end
   end
 
@@ -65,19 +65,18 @@ class ReservationsController < ApplicationController
       :id, :title, :description, :number_of_people, :start_date, :end_date, :hall_id)
   end
 
-  def premium_override(conflicting_reservations)
+  def premium_override
     if current_user.premium?
-      redirect_to reservations_path, alert: "This should be a pop up for prezes
+      redirect_to reservations_path, alert: "This should be a pop up for prezes,
       #{
-      conflicting_reservations.each.map(&:title)
+      @conflicting_reservations.each.map(&:title)
       }"
     else
       redirect_to reservations_path, alert: "Reservation conflict with
       #{
-      conflicting_reservations.each.map(&:title)
+      @conflicting_reservations.each.map(&:title)
       }"
     end
   end
-
 
 end
