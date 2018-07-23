968bc4f180d09b980d9c0c18a2885d931a2eb463
MrArthurr <chrzaszczewski.artur@gmail.com>
2018-07-23 11:59:25 +0200

    rename invited to invited_ids and inviter to inviter_ids in group model

--- db/migrate/20180723093819_rename_user_attributes.rb
+++ db/migrate/20180723093819_rename_user_attributes.rb
@@ -4,5 +4,7 @@ class RenameUserAttributes < ActiveRecord::Migration[5.2]
     rename_column :users, :is_admin, :admin
     rename_column :users, :is_verified, :verified
     rename_column :reservations, :invited, :invited_ids
+    rename_column :groups, :invited, :invited_ids
+    rename_column :groups, :inviter, :inviter_ids
   end
 end
