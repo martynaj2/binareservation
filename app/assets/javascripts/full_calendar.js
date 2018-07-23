0b817783b3181ab561aef11aa58997226b715376
martynaj2 <martyna.jasiak@gmail.com>
2018-07-23 19:14:34 +0200

    [add] connected date from calendar with reservation form

--- app/assets/javascripts/full_calendar.js
+++ app/assets/javascripts/full_calendar.js
@@ -11,7 +11,35 @@ initialize_calendar = function() {
       selectable: true,
       selectHelper: true,
       editable: true,
-      eventLimit: true
+      eventLimit: true,
+
+      dayClick: function(date) {
+
+        var start_date = date;
+        if(!start_date.hasTime()) {
+          start_date.time('12:00:00');
+        }
+        window.location.href='/reservations/new?start_date=' + start_date.format() + '&end_date=' + start_date.add('hours', 1).format();
+        $(this).css('background-color', 'purple');
+      },
+
+      select: function( start, end, jsEvent, view) {
+
+        $(".calendar").fullCalendar('addEventSource', [{
+            start: start,
+            end: end,
+            rendering: 'background',
+            block: true,
+        }, ]);
+
+        window.location.href='/reservations/new?start_date=' + start.format() + '&end_date=' + end.format();
+        $("#calendar").fullCalendar("unselect");
+
+        },
+        selectOverlap: function(event) {
+          return ! event.block;
+        },
+
     });
   })
 };
