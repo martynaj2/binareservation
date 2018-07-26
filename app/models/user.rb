1cec12bf1e21e46aec9c428f1eccb055446f1503
martynaj2 <martyna.jasiak@gmail.com>
2018-07-26 11:24:52 +0200

    [add] validation for email in registration form

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   validates :name, :surname, :email, presence: true
   validates :name, :surname, length: { minimum: 2, maximum: 15}
   validates :avatar, file_size: { less_than: 1.megabytes }
+  validates :email, length: { minimum: 2, maximum: 50 }
 
   has_many :reservations
   has_many :group_users
