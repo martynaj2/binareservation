47899f7c71a69746cda36ee812dbdbca36011d44
ptoiraleus <ptoiraleus@gmail.com>
2018-07-30 13:55:37 +0200

    [add] Added user controller test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -51,5 +51,61 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
-  
+  describe '#update' do
+    let(:user) {create(:user)}
+    before { sign_in(user) }
+    let(:valid_attributes) { { id: user.id, user: { name: 'Name', surname: 'Surname' } } }
+    let(:invalid_attributes) { { id: user.id, user: { name: nil, surname: nil } } }
+
+    context 'valid params' do
+      subject { patch :update, params: valid_attributes }
+
+      it 'should redirect to user index' do
+        expect(subject).to redirect_to(profile_path)
+      end
+
+      it 'should redirect with notice' do
+        subject
+        expect(flash[:notice]).to be_present
+      end
+
+      it 'should change user Name and Surname' do
+        subject
+        expect(user.reload.name).to eq( 'Name' )
+        expect(user.reload.surname).to eq( 'Surname' )
+      end
+    end
+
+    context 'invalid params' do
+      subject { patch :update, params: invalid_attributes }
+
+      it 'should render edit' do
+        expect(subject).to render_template('edit')
+      end
+
+      it 'should not change title' do
+        subject
+        expect(user.reload.name).not_to eq('Name')
+        expect(user.reload.surname).not_to eq( 'Surname' )
+      end
+    end
+  end
+
+  describe '#vacation' do
+    let(:user) {create(:user)}
+    before { sign_in(user) }
+    before { put :vacation, params: { id: user.id } }
+
+    context 'valid params' do
+
+      it 'should redirect to user index' do
+        expect(subject).to redirect_to(profile_path)
+      end
+
+      it 'should change user vacation status' do
+        subject
+        expect(user.reload.vacation).to eq( true )
+      end
+    end
+  end
 end
