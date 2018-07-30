43ae4654ca762c44b629232d2be4503430135f10
ptoiraleus <ptoiraleus@gmail.com>
2018-07-30 13:40:05 +0200

    pull

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-   let(:user1) { create(:user, email: 'b@wp.pl') }
+   let(:user1) { create(:user) }
    before { sign_in(user1) }
 
-   let(:user2) { create(:user, email: 'c@wp.pl') }
+   let(:user2) { create(:user) }
    before { sign_in(user2) }
 
   describe '#index' do
@@ -23,7 +23,33 @@ RSpec.describe UsersController, type: :controller do
         expect(assigns(:users)).to match_array([user1, user2])
       end
     end
-
   end
 
+  describe '#show' do
+    before { get :show, params: { id: user1.id } }
+
+    describe 'successful response' do
+      it { expect(response).to be_successful }
+      it { expect(response).to render_template('show') }
+    end
+
+    context 'user' do
+      it { expect(assigns(:user)).to eq(user1) }
+    end
+  end
+
+  describe '#edit' do
+    before { get :edit, params: { id: user2.id } }
+
+    describe 'successful response' do
+      it { expect(response).to be_successful }
+      it { expect(response).to render_template('edit') }
+    end
+
+    context 'user' do
+      it { expect(assigns(:user)).to eq(user2) }
+    end
+  end
+
+  
 end
