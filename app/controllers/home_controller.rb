b69f3035f5766030b75c29ec4b9b9dd11541ca27
martynaj2 <martyna.jasiak@gmail.com>
2018-07-30 11:46:10 +0200

    [fix] rubocop errors for home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,12 @@
 class HomeController < ApplicationController
-	def welcome
-		@rooml = Hall.large
-		@rooms = Hall.small
-		@roomm = Hall.medium
-		@roomxl = Hall.extra_large
-	end
+  def welcome
+    @rooml = Hall.large
+    @rooms = Hall.small
+    @roomm = Hall.medium
+    @roomxl = Hall.extra_large
+  end
 
-	def index
-		@reservations = Reservation.not_ended
-	end
+  def index
+    @reservations = Reservation.not_ended
+  end
 end
