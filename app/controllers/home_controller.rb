6020c16d760064e9593f6325afe6ce592402e44b
ptoiraleus <ptoiraleus@gmail.com>
2018-08-01 14:28:54 +0200

    refactor

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,4 @@
 class HomeController < ApplicationController
-  def welcome
-    @rooml = Hall.large
-    @rooms = Hall.small
-    @roomm = Hall.medium
-    @roomxl = Hall.extra_large
-    @reservations = Reservation.not_ended
-  end
-
   def index
     @rooml = Hall.large
     @rooms = Hall.small
