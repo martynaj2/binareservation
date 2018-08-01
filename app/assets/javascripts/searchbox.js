ac9d8c42c4578b428f0f89bafe0e585bd5f1c28a
MrArthurr <chrzaszczewski.artur@gmail.com>
2018-08-01 12:38:46 +0200

    [add] added empty state in search (reservation&group) and some css

--- app/assets/javascripts/searchbox.js
+++ app/assets/javascripts/searchbox.js
@@ -7,13 +7,19 @@ function FindUsers() {
     li = ul.getElementsByTagName('li');
 
 
-
     for (i = 0; i < li.length; i++) {
         a = li[i].getElementsByTagName("a")[0];
         if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "";
+            $(li[i]).addClass('user-group-list-elem');
         } else {
             li[i].style.display = "none";
+            $(li[i]).removeClass('user-group-list-elem');
         }
+    }
+    if ($('.user-group-list-elem').length == 0){
+      $("#empty-message").html("Ooops we can't find what you're looking for...");
+    } else {
+      $("#empty-message").html("");
     }
 }
