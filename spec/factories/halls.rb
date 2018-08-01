fe2e9ece167f09c238964e121f7cd0919cf2cc65
martynaj2 <martyna.jasiak@gmail.com>
2018-08-01 19:20:27 +0200

    [fix] tests for hall controller

--- spec/factories/halls.rb
+++ spec/factories/halls.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :hall do
-    title     { Faker::Name.first_name }
-    capacity  { Faker::Number.between(5, 20) }
+    title       { Faker::Name.first_name }
+    capacity    { Faker::Number.between(5, 20) }
+    hall_color  { Faker::Color.hex_color }
   end
 end
